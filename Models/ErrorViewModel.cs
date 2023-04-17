52ba262135cd7a04b2c2f923a42ab39c6de5230d
Nguyen Hoang <hoangforce@outlook.com>
2023-04-17 23:13:27 +0700

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DemoTest.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
